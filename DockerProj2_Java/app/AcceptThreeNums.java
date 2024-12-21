1382669d6d017fb18f342f960fc05bc3e409fc2d
gauravdkumar1211 <gauravdkumar1211@gmail.com>
2024-12-21 23:04:50 +0530

    Version 2.0

--- DockerProj2_Java/app/AcceptThreeNums.java
+++ DockerProj2_Java/app/AcceptThreeNums.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 public class AcceptThreeNums{
-    
+
+
+	public static int averageOfThreeNums(int no1,int no2,int no3){
+		return (no1+no2+no3)/3;
+	}
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -13,6 +17,8 @@ public class AcceptThreeNums{
 		System.out.println("Enter No3:");
 		int no3 = sc.nextInt();
 		System.out.println("Entered Numbers are:" +no1 +" "+no2+" "+" "+no3+" ");
+
+		System.out.println("Average of THree nums is "+averageOfThreeNums(no1,no2,no3));
 	}
 
 }
